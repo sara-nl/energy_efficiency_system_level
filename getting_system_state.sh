a3e1576aca8314460cc8f7eb3e5474e5de69a4ee
teimour-halizadeh <teimour.hosseinalizadeh@gmail.com>
2024-11-12 14:32:50 +0100

    data from previous state is available

--- getting_system_state.sh
+++ getting_system_state.sh
@@ -17,10 +17,25 @@ done
 
 for day in {1..30}   
 do
-    for i in {1..2880}  
+    for i in {1..1440}  
     do 
-        echo "############################" >> system_analytics_2024/slurm_data/system_states/system_states_${day}.txt
-        sinfo -l >> system_analytics_2024/slurm_data/system_states/system_states_${day}.txt   
-        sleep 30   
+        echo "############################" >> system_analytics_2024/slurm_data/system_states_int5/system_states_${day}.txt
+        sinfo -l >> system_analytics_2024/slurm_data/system_states_int5/system_states_${day}.txt   
+        sleep 60   
+    done
+done
+
+
+
+
+#!/bin/bash
+
+for day in {1..30}   
+do
+    for i in {1..720}  
+    do 
+        echo "############################" >> system_analytics_2024/slurm_data/system_states_int4/system_states_${day}.txt
+        sinfo -l >> system_analytics_2024/slurm_data/system_states_int4/system_states_${day}.txt   
+        sleep 120   
     done
 done
